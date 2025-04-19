e628029dab5058a627b6a760ce5cd87223d311bf
Anish <anissh946@gmail.com>
2025-04-20 01:32:56 +0530

    resolve error

--- next.config.ts
+++ next.config.ts
@@ -5,10 +5,26 @@ const nextConfig: NextConfig = {
     // Disables ESLint checks during the build process
     ignoreDuringBuilds: true,
   },
-  typescript : {
-    ignoreBuildErrors : true
-  }
-  /* Add other Next.js configuration options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "2mb", // example value
+      allowedOrigins: ["*"], // or your actual allowed origins
+    },
+  },
+  
+  webpack: (config, { isServer }) => {
+    config.resolve = {
+      ...config.resolve,
+      alias: {
+        ...(config.resolve.alias || {}),
+        'react-server-dom-turbopack/server.edge': 'react-server-dom-webpack/server.edge',
+      },
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
